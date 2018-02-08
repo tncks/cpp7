580caa409a802716ccca95439d9b2f267fb2c155
tncks <duddndtncks@naver.com>
2018-02-08 21:28:56 +0900

    dll making project

---
+++ dllProject/dlProject.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include <iostream>
+#include "dllmain.h"
+
+using namespace Hello;
+
+int main()
+{
+	Hello::printHello();
+	Hello::printGoodBye();
+	system("pause");
+	return 0;
+}
